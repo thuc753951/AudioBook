12de4dde7ab41108684bf5d232abcdcad9298861
Phuc <40041728+thuc753951@users.noreply.github.com>
2021-11-27 22:32:58 -0500

    stable build

--- app/src/main/java/edu/temple/audiobb/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobb/MainActivity.kt
@@ -1,32 +1,28 @@
 package edu.temple.audiobb
 
 import android.app.DownloadManager
-import android.content.Intent
-import android.content.SharedPreferences
+import android.content.*
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.util.Log
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import edu.temple.audlibplayer.PlayerService
+import java.io.File
 
 class MainActivity : AppCompatActivity(), BookListFragment.BookSelectedInterface, ControlFragment.ControlInterface {
 
+
+
     private lateinit var bookListFragment : BookListFragment
-    private lateinit var ControlFragment: ControlFragment
+    private lateinit var controlFragment: ControlFragment
 
-    private val DIR = "audioBook"
-    var Aid: Long = 0
-    lateinit var DownloadManager: DownloadManager
-    lateinit var Temp: String
-
-    var isDownloadDone = false
-    lateinit var mediaControl: PlayerService.MediaControlBinder
-    var serviceConnection = false
-    var isItSave = false
-
-    lateinit var preferences: SharedPreferences
 
 
     private val searchRequest = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
@@ -52,14 +48,14 @@ class MainActivity : AppCompatActivity(), BookListFragment.BookSelectedInterface
 
     companion object {
         const val BOOKLISTFRAGMENT_KEY = "BookListFragment"
+        const val CONTROLFRAGMENT_KEY = "ControlFragment"
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Grab test data
-        //getBookList()
 
         // If we're switching from one container to two containers
         // clear BookDetailsFragment from container1
@@ -70,9 +66,11 @@ class MainActivity : AppCompatActivity(), BookListFragment.BookSelectedInterface
 
         // If this is the first time the activity is loading, go ahead and add a BookListFragment
         if (savedInstanceState == null) {
+            controlFragment = ControlFragment()
             bookListFragment = BookListFragment()
             supportFragmentManager.beginTransaction()
                 .add(R.id.container1, bookListFragment, BOOKLISTFRAGMENT_KEY)
+                .add(R.id.control, controlFragment, CONTROLFRAGMENT_KEY)
                 .commit()
         } else {
             bookListFragment = supportFragmentManager.findFragmentByTag(BOOKLISTFRAGMENT_KEY) as BookListFragment
@@ -118,19 +116,22 @@ class MainActivity : AppCompatActivity(), BookListFragment.BookSelectedInterface
         }
     }
 
+
+
     override fun play() {
-        TODO("Not yet implemented")
+        Toast.makeText(this,"Pressed Play", Toast.LENGTH_SHORT).show()
     }
 
     override fun pause() {
-        TODO("Not yet implemented")
+        Toast.makeText(this,"Pressed Paused", Toast.LENGTH_SHORT).show()
     }
 
     override fun stop() {
-        TODO("Not yet implemented")
+        Toast.makeText(this,"Pressed Stop", Toast.LENGTH_SHORT).show()
     }
 
     override fun changeTime(position: Int) {
-        TODO("Not yet implemented")
+        Toast.makeText(this,"Changing Time", Toast.LENGTH_SHORT).show()
+
     }
 }
