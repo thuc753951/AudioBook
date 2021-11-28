4dc86455a5eb48852f5681052463b2dd685ece3e
Phuc <40041728+thuc753951@users.noreply.github.com>
2021-11-27 21:35:22 -0500

    testing 23

--- app/src/main/java/edu/temple/audiobb/ControlFragment.kt
+++ app/src/main/java/edu/temple/audiobb/ControlFragment.kt
@@ -1,59 +1,105 @@
 package edu.temple.audiobb
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ControlFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ControlFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var parentInterface: ControlInterface
+    lateinit var seekBar: SeekBar
+    lateinit var nowPlaying: TextView
+    lateinit var play: Button
+    lateinit var pause: Button
+    lateinit var stop: Button
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control, container, false)
+        val layout = inflater.inflate(R.layout.fragment_control, container, false)
+
+        play = layout.findViewById(R.id.play)
+        play.setOnClickListener {
+            parentInterface.play()
+        }
+        pause = layout.findViewById(R.id.pause)
+        pause.setOnClickListener {
+            parentInterface.pause()
+        }
+        stop = layout.findViewById(R.id.stopButton)
+        stop.setOnClickListener {
+            parentInterface.stop()
+        }
+        nowPlaying = layout.findViewById(R.id.nowPlaying)
+
+        seekBar = layout.findViewById(R.id.seekBar)
+
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                if (b) parentInterface.changeTime(i)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+
+        return layout
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ControlFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ControlFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun setPlaying(title: String){
+        nowPlaying.text = title
     }
+    fun updateProgress(progress: Int){
+        seekBar.progress = progress
+    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is ControlInterface) parentInterface =
+            context as ControlInterface else throw RuntimeException("Please implement ControlFragment.ControlInterface")
+    }
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment ControlFragment.
+//         */
+//
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            ControlFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+    interface ControlInterface {
+        fun play()
+        fun pause()
+        fun stop()
+        fun changeTime(position: Int)
+}
 }
